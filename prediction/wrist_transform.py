a63fc0fc8e12eec7b6b5ec5e6313d599f4337e15
Leon998 <1158046368@qq.com>
2023-06-12 17:22:02 +0800

    wrist control

--- prediction/wrist_transform.py
+++ prediction/wrist_transform.py
@@ -14,7 +14,7 @@ if __name__ == "__main__":
     # Source files
     source_files = os.listdir(path)
     source_files.sort()
-    idx = 300 # 随便选的一个抓取的序号
+    idx = 150 # 随便选的一个抓取的序号
     file = source_files[idx]
     # Coordinate
     coordinate = o3d.geometry.TriangleMesh.create_coordinate_frame(size=0.15, origin=[0, 0, 0])
@@ -60,11 +60,73 @@ if __name__ == "__main__":
     # q_transform = R.from_matrix(r_transform).as_quat()
     euler_transform = R.from_matrix(r_transform).as_euler('zyx', degrees=True)
     print(euler_transform)
+    wrist_rotate = euler_transform[0]
+    wrist_flip = euler_transform[2]
 
     transformed_hand = copy.deepcopy(mid_hand)
     transformed_hand.rotate(r_transform, center=mid_pose[4:])
     transformed_hand.paint_uniform_color([255 / 255, 190 / 255, 122 / 255])
     meshes.append(transformed_hand)
+    
 
+    # ============================ Wrist control ======================= #
+    from ctypes import *
+ 
+    VCI_USBCAN2 = 4
+    STATUS_OK = 1
+    class VCI_INIT_CONFIG(Structure):  
+        _fields_ = [("AccCode", c_uint),
+                ("AccMask", c_uint),
+                ("Reserved", c_uint),
+                ("Filter", c_ubyte),
+                ("Timing0", c_ubyte),
+                ("Timing1", c_ubyte),
+                ("Mode", c_ubyte)
+                ]  
+    class VCI_CAN_OBJ(Structure):  
+        _fields_ = [("ID", c_uint),
+                ("TimeStamp", c_uint),
+                ("TimeFlag", c_ubyte),
+                ("SendType", c_ubyte),
+                ("RemoteFlag", c_ubyte),
+                ("ExternFlag", c_ubyte),
+                ("DataLen", c_ubyte),
+                ("Data", c_ubyte*8),
+                ("Reserved", c_ubyte*3)
+                ] 
+ 
+    CanDLLName = 'can/ControlCAN.dll' #把DLL放到对应的目录下
+    canDLL = windll.LoadLibrary(CanDLLName)
 
+ 
+    ret = canDLL.VCI_OpenDevice(VCI_USBCAN2, 0, 0)
+ 
+    #初始0通道
+    vci_initconfig = VCI_INIT_CONFIG(0x80000008, 0xFFFFFFFF, 0,
+                                 0, 0x00, 0x14, 0)#波特率1000k，正常模式
+    ret = canDLL.VCI_InitCAN(VCI_USBCAN2, 0, 0, byref(vci_initconfig))
+ 
+    ret = canDLL.VCI_StartCAN(VCI_USBCAN2, 0, 0)
+ 
+    #初始1通道
+    ret = canDLL.VCI_InitCAN(VCI_USBCAN2, 0, 1, byref(vci_initconfig))
+ 
+    ret = canDLL.VCI_StartCAN(VCI_USBCAN2, 0, 1)
+ 
+    #通道1发送数据
+    ubyte_array = c_ubyte*8
+    wrist_rotate = - int(wrist_rotate * 255 / 180) + 128
+    wrist_flip = - int(wrist_flip * 255 / 90) + 128
+    a = ubyte_array(0, wrist_rotate, wrist_flip, 0, 0, 0, 0, 0)
+    ubyte_3array = c_ubyte*3
+    b = ubyte_3array(0, 0 , 0)
+    vci_can_obj = VCI_CAN_OBJ(0x14, 0, 0, 1, 0, 0,  8, a, b)#单次发送，0x14为手腕id
+ 
+    ret = canDLL.VCI_Transmit(VCI_USBCAN2, 0, 0, byref(vci_can_obj), 1)
+ 
+
+    #关闭
+    canDLL.VCI_CloseDevice(VCI_USBCAN2, 0) 
+
+    
     o3d.visualization.draw_geometries(meshes)
